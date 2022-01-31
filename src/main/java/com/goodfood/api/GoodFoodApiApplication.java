fb22a445ba3c8bb6c1f1050793d0f99fe33dc61f
Gaëtan TALTAVULL <gaetan.taltavull@gmail.com>
2022-01-31 23:42:03 +0100

    Initalize project

---
+++ src/main/java/com/goodfood/api/GoodFoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.goodfood.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodFoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodFoodApiApplication.class, args);
+    }
+
+}
