69393decd28301653937e4330448ec943457f307
Gaëtan TALTAVULL <gaetan.taltavull@gmail.com>
2022-02-02 11:05:01 +0100

    Add configuration of swagger in application properties Add java class SwaggerConfig for to use the swagger V2 Resolve beug : Failed to start bean 'documentationPluginsBootstrapper' in spring data rest I have add this line for to resolve the beug : spring.mvc.pathmatch.matching-strategy = ANT_PATH_MATCHER

--- src/main/java/com/goodfood/api/GoodFoodApiApplication.java
+++ src/main/java/com/goodfood/api/GoodFoodApiApplication.java
@@ -1,34 +1,21 @@
 package com.goodfood.api;
 
-import com.goodfood.api.dao.EmployeesDAO;
-import com.goodfood.api.model.Employees;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.UUID;
 
 @SpringBootApplication
-public class GoodFoodApiApplication implements CommandLineRunner
+@EnableSwagger2
+public class GoodFoodApiApplication
 {
-    @Autowired
-    EmployeesDAO employeesDAO;
-
     public static void main(String[] args)
     {
         SpringApplication.run(GoodFoodApiApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception
-    {
-        employeesDAO.save(new Employees((int) UUID.randomUUID().getMostSignificantBits(), (int) UUID.randomUUID().getMostSignificantBits(), (int) UUID.randomUUID().getMostSignificantBits(), (int) UUID.randomUUID().getMostSignificantBits(), (int) UUID.randomUUID().getMostSignificantBits(),
-                "gaetan","taltavull","t","gaetan.taltavull@gmail.com","1",5));
-
-        for (Employees employees: employeesDAO.findAll())
-        {
-            System.out.println(employees.getFirstname());
-        }
-    }
 }
