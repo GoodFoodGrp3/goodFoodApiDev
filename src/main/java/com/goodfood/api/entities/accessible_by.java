54359e61cdba2440e997e5d01994b7434440fb13
Gaëtan TALTAVULL <gaetan.taltavull@gmail.com>
2022-02-02 12:22:55 +0100

    Add javax.persistence dependency in pom.xml Add Gson dependency for serialization in pom.xml
    I started the entity accessible_by.java

--- src/main/java/com/goodfood/api/entities/accessible_by.java
+++ src/main/java/com/goodfood/api/entities/accessible_by.java
@@ -1,6 +1,14 @@
 package com.goodfood.api.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import javax.persistence.Entity;
+import java.sql.Timestamp;
+
+@Entity
+//Classe à terminer
 public class accessible_by
 {
-
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", timezone = "Europe/Paris")
+    private Timestamp date;
 }
