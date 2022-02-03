ad73d27af27e94d362c6e8da51fa2ac84ad217a9
Gaëtan TALTAVULL <gaetan.taltavull@gmail.com>
2022-02-03 15:07:49 +0100

    Add java class error_log for to stock error in bdd

---
+++ src/main/java/com/goodfood/api/entities/error_log.java
@@ -0,0 +1,83 @@
+package com.goodfood.api.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.http.HttpStatus;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table( name = "error_log" )
+public class error_log {
+
+    @Column( name = "error_log_id" )
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO )
+    private int error_log_id;
+
+    @Column( name = "date" )
+    @JsonFormat( shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", timezone = "Europe/Paris" )
+    private Timestamp date;
+
+    @Column( name = "source" )
+    private String source;
+
+    @Column( name = "error_status" )
+    @Enumerated( EnumType.STRING )
+    private HttpStatus error_status;
+
+    @Column( name = "message" )
+    private String  message;
+
+    public error_log() {
+        this.date = new Timestamp( System.currentTimeMillis() );
+    }
+
+    public error_log( String source, HttpStatus errorStatus, String message ) {
+        super();
+        this.date = new Timestamp( System.currentTimeMillis() );
+        this.source = source;
+        this.error_status = errorStatus;
+        this.message = message;
+    }
+
+    public int getError_log_id() {
+        return error_log_id;
+    }
+
+    public void setError_log_id(int error_log_id) {
+        this.error_log_id = error_log_id;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public HttpStatus getErrorStatus() {
+        return error_status;
+    }
+
+    public void setErrorStatus(HttpStatus errorStatus) {
+        this.error_status = errorStatus;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
