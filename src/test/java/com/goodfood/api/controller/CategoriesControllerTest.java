b37ecfeda70ae2fca0e90b9f2e7f4ee64b2661ba
Gaëtan <gaetan.taltavull@gmail.com>
2022-03-19 18:15:28 +0100

    Ajout de la dépendance Junit pour faire les tests d'intégration sur les controllers
    Ajout des class tests : CategoriesControllerTest, CommentsControllerTest, CommoditysControllerTest, OrdersControllerTest pour vérifier que l'on reçoit bien le status 200 lors de l'accès à chaque route.

--- src/test/java/com/goodfood/api/controller/CategoriesControllerTest.java
+++ src/test/java/com/goodfood/api/controller/CategoriesControllerTest.java
@@ -6,23 +6,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CommentsControllerTest {
+public class CategoriesControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void testGetAllComment() throws Exception {
+    public void testGetAllCategories() throws Exception {
 
-        mockMvc.perform(get("/comments")).andExpect(status().isOk());
+        mockMvc.perform(get("/categories")).andExpect(status().isOk());
 
     }
-
 }
--- src/test/java/com/goodfood/api/controller/OrdersControllerTest.java
+++ src/test/java/com/goodfood/api/controller/OrdersControllerTest.java
@@ -6,23 +6,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CommentsControllerTest {
+public class OrdersControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void testGetAllComment() throws Exception {
+    public void testGetAllOrders() throws Exception {
 
-        mockMvc.perform(get("/comments")).andExpect(status().isOk());
+        mockMvc.perform(get("/orders")).andExpect(status().isOk());
 
     }
-
 }
